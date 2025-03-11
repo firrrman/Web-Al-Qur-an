5f78c6020317c229cc3b31dcef9a4e1384a04199
Firman Hakim <firmanhakim11704@gmail.com>
2025-03-11 11:17:23 +0700

    menambahkan configurasi base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "./", // Pengaturan base path
+  build: {
+    assetsInlineLimit: 0, // Mencegah konversi gambar kecil ke base64
+  },
 });
